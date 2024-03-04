617a9d8029f30b546c75fe87cc2f959ce7b79b64
Alvaro <alm2297@columbia.edu>
2024-03-04 11:00:28 -0500

    Add ranking to schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS clan_stats (
     clan_id TEXT,
-    timestamp TEXT,
+    timestamp TIMESTAMP,
+    ranking INTEGER,
     score REAL,
     PRIMARY KEY (clan_id, timestamp)
 );
